e9a4ddf821b939602afb692b6443dfedce2025e8
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2025-11-15 12:00:00 +0100

    improve code

--- db.sql
+++ db.sql
@@ -4,6 +4,7 @@ USE smart_wallet;
 CREATE TABLE incomes(
 	id int auto_increment primary key,
     amount DOUBLE NOT NULL,
+    
     date_income DATE NOT NULL DEFAULT (CURDATE()),
     destination enum("freelance", "salary", "buisness", "transfert") NOT NULL,
     description TEXT NOT NULL
@@ -15,4 +16,5 @@ CREATE TABLE expenses(
     date_expense DATE NOT NULL DEFAULT (CURDATE()),
     destination enum('shoping','food','bills','transport','trips')NOT NULL,
     description TEXT NOT NULL
+
 );
